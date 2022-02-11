45c3107201be9e5cd1224639eea577692b6a6c05
krisbitney <kristoferbitney@hotmail.com>
2022-02-11 11:35:30 -0300

    docgen: set up docgen to work with changes following "dapp" command merge. added docusaurus-react template.

--- packages/cli/src/lib/codegen-templates/docgen/docusaurus/docusaurus.gen.js
+++ packages/cli/src/lib/codegen-templates/docgen/docusaurus/docusaurus.gen.js
@@ -1,14 +1,40 @@
+
 exports.run = (output, config) => {
-  output.entries.push({
-    type: "File",
-    name: "./queries.md",
-    data: config.generate("docusaurus-queries.mustache", config.typeInfo),
+
+  config.typeInfo.title = () => {
+    return (value, render) => {
+      let rendered = render(value);
+      return rendered.charAt(0).toUpperCase() + rendered.substring(1);
+    }
+  }
+  config.typeInfo.lowerFirst = () => {
+    return (value, render) => {
+      let rendered = render(value);
+      return rendered.charAt(0).toLowerCase() + rendered.substring(1);
+    }
+  }
+
+  const queryContext = config.typeInfo.moduleTypes.find(def => {
+    return def.type === "Query";
   });
-  output.entries.push({
-    type: "File",
-    name: "./mutations.md",
-    data: config.generate("docusaurus-mutations.mustache", config.typeInfo),
+  const mutationContext = config.typeInfo.moduleTypes.find(def => {
+    return def.type === "Mutation";
   });
+
+  if (queryContext) {
+    output.entries.push({
+      type: "File",
+      name: "./queries.md",
+      data: config.generate("docusaurus-modules.mustache", { ...queryContext, ...config.typeInfo }),
+    });
+  }
+  if (mutationContext) {
+    output.entries.push({
+      type: "File",
+      name: "./mutations.md",
+      data: config.generate("docusaurus-modules.mustache", { ...mutationContext, ...config.typeInfo }),
+    });
+  }
   output.entries.push({
     type: "File",
     name: "./types.md",
